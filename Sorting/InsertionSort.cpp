bda904cfc8bd98f5e06c52d381d369611a79655f
Vilsi Jain <jainvilsi12@gmail.com>
2025-03-13 21:01:14 +0530

    Create InsertionSort.cpp

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,34 @@
+// Online C++ compiler to run C++ program online
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void insertionsort(int arr[],int n){
+    for(int i=0;i<=n-1;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+            j--;
+        }
+    }
+    cout<<"Insertion Sort:"<<"\n";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+    }
+int main()
+{
+    int arr[]={4,7,3,22,56};
+    int n=sizeof(arr) / sizeof(arr[0]);
+    cout<<"In Insertion Sort"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    insertionsort(arr,n);
+    return 0;
+}
